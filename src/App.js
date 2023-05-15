16be1a03c868fee395c3b5107cb7d3096a61aefd
AmineAmri24 <amineamri2495@gmail.com>
2023-05-15 10:47:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Person from './Component/Person'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {show:false}
+  }
+  handleClick = () => this.setState({show: !this.state.show})
+  render() {
+    return (
+      <div className='content'>
+        <button className='button-49' onClick={this.handleClick}>Toggle</button>
+        {this.state.show?<Person/>:null}
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/Component/Person.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class Person extends Component {
+    constructor(props){
+        super(props);
+        this.Person ={
+            fullname : "Amine",
+            Bio : "Engineer",
+            Profession : "FS JS for now",
+            img : "https://www.w3schools.com/howto/img_avatar.png"  
+        }
+        this.state = {count:0}
+    }
+  componentDidMount(){
+      setInterval(()=>{
+          this.setState(prevState=>({
+              count: prevState.count+1
+          }))
+      }, 1000)
+  }
+  componentWillUnmount(){
+      clearInterval(this.state)
+    }
+  render() {
+    return (
+      <div>
+       <h2>{this.Person.fullname}</h2>
+       <h2>{this.Person.Bio}</h2> 
+       <h2>{this.Person.Profession}</h2> 
+       <img src ={this.Person.img} alt = "profilpict" />
+       <div>Compteur:{this.state.count}</div>
+      </div>
+    )
+  }
+}
